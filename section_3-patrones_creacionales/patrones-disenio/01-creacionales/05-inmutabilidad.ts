1465c220d885c8c3e9841f89de1e9b753e1a3a62
Adriano Ayala <adriano.ayc@gmail.com>
2025-06-10 17:53:58 -0400

    Inmutabilidad con copia - Patrón

---
+++ section_3-patrones_creacionales/patrones-disenio/01-creacionales/05-inmutabilidad.ts
@@ -0,0 +1,77 @@
+/**
+ * ! Inmutabilidad con copia
+ * Aunque la inmutabilidad es una buena práctica, no siempre es posible.
+ * En estos casos, se puede hacer una copia del objeto y modificar la copia.
+ *
+ * * Es útil para mantener un historial de estados en aplicaciones interactivas.
+ */
+
+import { COLORS } from '../helpers/colors.ts';
+
+class CodeEditorState {
+  readonly content: string;
+  readonly cursorPosition: number;
+  readonly unsaveChanges: boolean;
+
+  constructor(
+    content: string,
+    cursorPosition: number,
+    unsavedChanges: boolean
+  ) {
+    this.content = content;
+    this.cursorPosition = cursorPosition;
+    this.unsaveChanges = unsavedChanges;
+  }
+
+  copyWith({
+    content,
+    cursorPosition,
+    unsaveChanges,
+  }: Partial<CodeEditorState>): CodeEditorState {
+    return new CodeEditorState(
+      content ?? this.content,
+      cursorPosition ?? this.cursorPosition,
+      unsaveChanges ?? this.unsaveChanges
+    );
+  }
+
+  displayState() {
+    console.log('\n%cEstado del editor:', COLORS.green);
+    console.log(`
+        Content: ${this.content}
+        Cursor Position: ${this.cursorPosition}
+        Unsaved Changes: ${this.unsaveChanges}
+        `);
+  }
+}
+
+class CodeEditorHistory {
+  private history: CodeEditorState[] = [];
+  private currentIndex: number = -1; // 0,1,2,3,4,5,6
+
+  save(state: CodeEditorState): void {
+    if (this.currentIndex < this.history.length - 1) {
+      this.history = this.history.slice(0, this.currentIndex + 1);
+    }
+    this.history.push(state);
+    this.currentIndex++;
+  }
+
+  undo(): CodeEditorState | null {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+      return this.history[this.currentIndex];
+    }
+
+    return null; // (0),1,2,3,4,5
+  }
+
+  redo(): CodeEditorState | null {
+    if (this.currentIndex < this.history.length - 1) {
+      this.currentIndex++;
+      return this.history[this.currentIndex];
+    }
+
+    return null; // 0,1,2,3,4,(5)
+  }
+}
