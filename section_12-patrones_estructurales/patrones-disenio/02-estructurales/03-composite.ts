f4e5b76c8f47e33dd4abe867c321aee1d69081c2
Adriano Ayala <adriano.ayc@gmail.com>
2025-06-25 16:12:09 -0400

    Composite - Utilización

--- section_12-patrones_estructurales/patrones-disenio/02-estructurales/03-composite.ts
+++ section_12-patrones_estructurales/patrones-disenio/02-estructurales/03-composite.ts
@@ -40,8 +40,39 @@ class Folder implements FileSystemComponent {
     this.contents.push(component);
   }
 
-  showDetails(indent?: string): void {
+  showDetails(indent: string = ''): void {
     console.log(`${indent}+ Carpeta: ${this.name}`);
     this.contents.forEach((component) => component.showDetails(indent + ' '));
   }
 }
+
+function main() {
+  const file1 = new File('archivo1.txt');
+  const file2 = new File('archivo2.txt');
+  const file3 = new File('archivo3.txt');
+  const file4 = new File('archivo4.txt');
+
+  const folder1 = new Folder('Carpeta 1');
+  folder1.add(file1);
+  folder1.add(file2);
+
+  const folder2 = new Folder('Carpeta 2');
+  folder2.add(file3);
+
+  const folder3 = new Folder('Carpeta 3');
+  folder3.add(file4);
+
+  folder2.add(folder3);
+
+  const folder5 = new Folder('Carpeta 5');
+
+  folder2.add(folder5);
+
+  const rootFolder = new Folder('Carpeta ROOT');
+  rootFolder.add(folder1);
+  rootFolder.add(folder2);
+
+  rootFolder.showDetails();
+}
+
+main();
