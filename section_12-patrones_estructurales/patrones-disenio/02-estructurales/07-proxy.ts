57f088be5df1b805d4b727ba3a967088123b9d46
Adriano Ayala <adriano.ayc@gmail.com>
2025-07-05 17:24:30 -0400

    Proxy - Patrón

---
+++ section_12-patrones_estructurales/patrones-disenio/02-estructurales/07-proxy.ts
@@ -0,0 +1,54 @@
+/**
+ * ! Patrón Proxy
+ * Este patrón se utiliza para controlar el acceso a un objeto, es decir, se cre un objeto que
+ * actúa como intermediario entre el cliente y el objeto real.
+ *
+ * * Es útil cuando necesitamos controlar el acceso a un objeto, por ejemplo, para verificar si
+ * * el cliente tiene permiso para acceder a ciertos métodos o propiedades.
+ *
+ *  https://refactoring.guru/es/design-patterns/proxy
+ */
+
+import { COLORS } from '../helpers/colors.ts';
+
+class Player {
+  name: string;
+  level: number;
+
+  constructor(name: string, level: number) {
+    this.name = name;
+    this.level = level;
+  }
+}
+
+interface Room {
+  enter(player: Player): void;
+}
+
+class SecretRoom implements Room {
+  enter(player: Player): void {
+    console.log(`%cBienvenido a la sala secreta, ${player.name}`, COLORS.blue);
+    console.log('Un gran enemigo te espera');
+  }
+}
+
+// 3. Clase Proxy - Magic Portal
+class MagicPortal implements Room {
+  private secretRoom: Room;
+
+  constructor(room: SecretRoom) {
+    this.secretRoom = room;
+  }
+
+  enter(player: Player): void {
+    if (player.level >= 10) {
+      this.secretRoom.enter(player);
+      return;
+    }
+
+    console.log(
+      `%cLo siento mucho ${player.name}, tu nivel ${player.level}, es muy bajo, ncesistas nivel 10`,
+      COLORS.red
+    );
+  }
+}
