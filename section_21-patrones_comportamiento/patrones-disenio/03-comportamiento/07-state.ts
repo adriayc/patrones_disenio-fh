410e662dc58ce2736657c303834fa8ee7c77fa47
Adriano Ayala <adriano.ayc@gmail.com>
2025-08-21 18:26:04 -0400

    State - Patrón (Parte 1)

---
+++ section_21-patrones_comportamiento/patrones-disenio/03-comportamiento/07-state.ts
@@ -0,0 +1,83 @@
+/**
+ * ! Patrón State
+ * Este patrón permite a un objecto cambiar su comportamiento cuando su estado interno cambia.
+ *
+ * * Es útil cuando un objeto tiene un comportamiento que depende de su estado y debe cambiar
+ * * su comportamiento en tiempo de ejecución dependiendo de ese estado.
+ *
+ * https://refactoring.guru/es/design-patterns/state
+ */
+
+import { COLORS } from '../helpers/colors.ts';
+
+/**
+ * Objetivo: Implementar el patrón State para simular el funcionamiento de una máquina expendedora.
+ *  La máquina tiene diferentes estados,
+ *   Como Esperando Dinero,
+ *   Seleccionando Producto,
+ *   Entregando Producto,
+ *  y su comportamiento varía dependiendo del estado actual.
+ */
+
+interface State {
+  name: string;
+
+  insertMoney(): void;
+  selectProduct(): void;
+  dispenseProduct(): void;
+}
+
+class VendingMachine {
+  private state: State;
+
+  constructor() {
+    // TODO: estado inicial
+  }
+
+  insertMoney() {
+    this.state.insertMoney();
+  }
+
+  selectProduct() {
+    this.state.selectProduct();
+  }
+
+  dispenseProduct() {
+    this.state.dispenseProduct();
+  }
+
+  setState(newState: State) {
+    this.state = newState;
+    console.log(`Estado cambió a: %c${newState.name}`, COLORS.yellow);
+  }
+
+  getStateName() {
+    return this.state.name;
+  }
+}
+
+// Estados
+class WaitingForMoney implements State {
+  public name: string = 'Esperando Dinero';
+  private vendingMachine: VendingMachine;
+
+  constructor(vendingMachine: VendingMachine) {
+    this.vendingMachine = vendingMachine;
+  }
+
+  insertMoney(): void {
+    console.log(
+      'Dinero insertado. %cAhora puedes seleccionar un producto',
+      COLORS.green
+    );
+
+    // TODO:
+    // this.vendingMachine.setState();
+  }
+  selectProduct(): void {
+    console.log(`%cPrimero debes de insertar dinero.`, COLORS.red);
+  }
+  dispenseProduct(): void {
+    console.log(`%cPrimero debes de insertar dinero.`, COLORS.red);
+  }
+}
